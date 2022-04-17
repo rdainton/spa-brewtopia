3d37471538285347a4fbc1e56c26e6f97d418050
Ryan Dainton <ryandainton@live.com>
2022-04-17 16:29:46 +0100

    logo change

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,10 +1,12 @@
 <template>
   <img
-    src="logo-500-dark-web.png"
+    src="@/assets/logo-500-dark-web.png"
+    alt="Brewtopia Logo"
     class="hidden w-64 h-auto max-w-full pt-3 dark:block"
   />
   <img
-    src="logo-500-standard-web.png"
+    src="@/assets/logo-500-standard-web.png"
+    alt="Brewtopia Logo"
     class="w-64 h-auto max-w-full pt-3 dark:hidden"
   />
 </template>
